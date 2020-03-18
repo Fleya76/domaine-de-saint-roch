6c715ceea7d1bc056b5ad24247052d35068fc2ae
Fabien Poret <fabien.poret@outlook.fr>
2020-03-18 20:27:57 +0100

    Ajout de la connexion

--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PlanningController extends AbstractController
 {
     /**
      * @Route("/", name="planning")
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
      */
     public function index()
     {
